e0bb4bfd8ea5ef90f40537015035d4dca08b0ea4
Pablo Silva <pablosilvaaluno@outlook.com>
2024-12-27 15:48:10 -0300

    Install mongoose and connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
-const app = express()
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001')
-})
+  connectDB();
+  console.log("Server is running on http://localhost:3001");
+});
