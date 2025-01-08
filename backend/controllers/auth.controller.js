0873106968181ad163211054514198bc5385815e
Pablo Silva <pablosilvaaluno@outlook.com>
2025-01-08 10:49:36 -0300

    feat: System login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -75,20 +75,49 @@ export const verifyEmail = async (req, res) => {
 
     await sendWelcomeEmail(user.email, user.name);
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Email verified",
-        user: { ...user._doc, password: undefined },
-      });
+    res.status(200).json({
+      success: true,
+      message: "Email verified",
+      user: { ...user._doc, password: undefined },
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
 };
 
 export const login = async (req, res) => {
-  res.send("Login Page");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: { ...user._doc, password: undefined },
+    });
+  } catch (error) {
+    console.log("Error in login", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
