230e4a3df3d4c339ca5844f9842cfc56f216a985
ppablolds <pablosilvaaluno@outlook.com>
2025-01-14 11:12:50 -0300

    feat: check Auth

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -30,6 +30,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/prop-types': 'off',
       'react/no-unescaped-entities': 'off',
+      'react/no-unusued-vars': 'off',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
