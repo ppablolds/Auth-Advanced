a05e6863b9d01e12479067e039f9dcfde68a607c
ppablolds <pablosilvaaluno@outlook.com>
2025-01-14 10:08:58 -0300

    refact code indentation

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -42,20 +42,35 @@ const LoginPage = () => {
           />
 
           <div className="flex items-center mb-6">
-            <Link to="/forgot-password" className="text-sm text-green-400 hover:underline">
+            <Link
+              to="/forgot-password"
+              className="text-sm text-green-400 hover:underline"
+            >
               Forgot Password?
             </Link>
           </div>
 
-          <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover-from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200" whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }} disabled={isLoading} type="submit">
-            {isLoading ? <Loader className="w-6 h-6 animate-spin text-center mx-auto" /> : "Login"}
+          <motion.button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover-from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            disabled={isLoading}
+            type="submit"
+          >
+            {isLoading ? (
+              <Loader className="w-6 h-6 animate-spin text-center mx-auto" />
+            ) : (
+              "Login"
+            )}
           </motion.button>
         </form>
       </div>
       <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
         <p className="text-sm text-gray-400">
-            Don't have an Account?{" "}
-            <Link to={"/signup"} className="text-green-400 hover:underline">SignUp</Link>
+          Don't have an Account?{" "}
+          <Link to={"/signup"} className="text-green-400 hover:underline">
+            SignUp
+          </Link>
         </p>
       </div>
     </motion.div>
